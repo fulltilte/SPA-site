bf924facd511cfb5952937c08a7a3755cd7a4ef9
Ivan Shvedov <d4zzy@bk.ru>
2024-05-22 23:23:19 +0300

    timer fixed

--- js/timer.js
+++ js/timer.js
@@ -10,9 +10,7 @@ function formatTime(seconds) {
 
 function updateTimer() {
     let currentTime = new Date().getTime();
-    let timeOnSite = currentTime - startTime;
-
-    timeOnSite = Math.floor(timeOnSite / 1000);
+    let timeOnSite = Math.floor((currentTime - window.startTime) / 1000);
     let formattedTime = formatTime(timeOnSite);
 
     let timerElement = document.getElementById('timer');
@@ -21,21 +19,14 @@ function updateTimer() {
     }
 }
 
-let startTime = localStorage.getItem('startTime');
-
-if (!startTime) {
-    startTime = new Date().getTime();
-    localStorage.setItem('startTime', startTime);
-}
-
-startTime = parseInt(startTime);
+window.startTime = window.startTime || new Date().getTime();
 
 setInterval(updateTimer, 100);
 
 window.onfocus = function() {
-    localStorage.setItem('startTime', new Date().getTime());
+    window.startTime = new Date().getTime();
 };
 
 window.addEventListener('beforeunload', function() {
-    localStorage.removeItem('startTime');
+    window.startTime = 0;
 });
