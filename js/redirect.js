942d60c9fd674313e2ad1742b4b1a4119df4c42f
Ivan Shvedov <d4zzy@bk.ru>
2024-05-23 23:53:11 +0300

    fixed

--- js/redirect.js
+++ js/redirect.js
@@ -1,10 +1,5 @@
-(function(l) {
-    if (l.search[1] === '/' ) {
-      var decoded = l.search.slice(1).split('&').map(function(s) { 
-        return s.replace(/~and~/g, '&')
-      }).join('?');
-      window.history.replaceState(null, null,
-          l.pathname.slice(0, -1) + decoded + l.hash
-      );
-    }
-  }(window.location))
+function handleRedirect() {
+    var redirectPath = window.location.pathname.replace('/your-repo-name/', '/');
+    window.location.replace("/?" + redirectPath);
+}
+handleRedirect();
--- js/spa.js
+++ js/spa.js
@@ -2,15 +2,30 @@ document.addEventListener('DOMContentLoaded', () => {
     renderPage();
 
     window.addEventListener('popstate', renderPage);
+
+    document.getElementById('act-link').addEventListener('click', (event) => {
+        event.preventDefault();
+        navigateTo('/');
+    });
+
+    document.getElementById('map-link').addEventListener('click', (event) => {
+        event.preventDefault();
+        navigateTo('/map');
+    });
+
+    document.getElementById('timer-link').addEventListener('click', (event) => {
+        event.preventDefault();
+        navigateTo('/timer');
+    });
 });
 
 const renderPage = () => {
-    const path = window.location.pathname;
+    const path = window.location.search.substring(1);
 
     const routes = {
         '': '/activity.html',
-        '/map': '/map.html',
-        '/timer': '/timer.html'
+        'map': '/map.html',
+        'timer': '/timer.html'
     };
 
     const pagePath = routes[path] || '/activity.html';
@@ -31,21 +46,6 @@ const renderPage = () => {
 };
 
 const navigateTo = (path) => {
-    history.pushState(null, null, path);
+    history.pushState(null, null, `?${path}`);
     renderPage();
 };
-
-document.getElementById('act-link').addEventListener('click', (event) => {
-    event.preventDefault();
-    navigateTo('/');
-});
-
-document.getElementById('mapButton').addEventListener('click', (event) => {
-    event.preventDefault();
-    navigateTo('/map');
-});
-
-document.getElementById('timer-link').addEventListener('click', (event) => {
-    event.preventDefault();
-    navigateTo('/timer');
-});
